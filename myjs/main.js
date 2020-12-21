5a2a622a034a718fe74f16f6dcbde67f98376f81
nohsangwoo <nsgr12@naver.com>
2020-12-22 04:16:03 +0900

    스크롤 처리 기본 개념 잡기

--- myjs/main.js
+++ myjs/main.js
@@ -1,4 +1,5 @@
 (() => {
+  let yOffset = 0; //window.pageYOffset 대신 쓸 변수
   const sceneInfo = [
     {
       // 0
@@ -51,7 +52,17 @@
     console.log(sceneInfo);
 
     // window의 크기가 변하면(사용자 기기의 브라우저의 크기를 임의로 변경시) setLayout을 실행해준다(창의 높이를 재설정해준다)
-    window.addEventListener("resize", setLayout());
+    window.addEventListener("resize", setLayout);
+
+    function scrollLoop() {
+      // 현재 스크롤의 위치를 가져옴
+      console.log(yOffset);
+    }
+    // 스크롤될때 이벤트 작동
+    window.addEventListener("scroll", () => {
+      yOffset = window.pageYOffset;
+      scrollLoop();
+    });
   }
   setLayout();
 })();
